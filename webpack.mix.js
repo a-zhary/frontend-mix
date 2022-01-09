6fd69a3e799b36921b913f52c4e39768daba2bb9
Anton Zhary <anton.zhary@gmail.com>
2022-01-09 07:26:00 +0300

    Add sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('mix-html-builder');
 
 /*
  |--------------------------------------------------------------------------
@@ -26,9 +27,30 @@ mix
 		injectChanges: true,
 		open: true,
 	})
-	.sass('resources/scss/main.scss', 'css')
-	.js('resources/js/app.js', 'js')
-	.copy('resources/index.html', 'public/')
+	.sass('resources/scss/main.scss', 'css/main.min.css')
+	.js('resources/js/app.js', 'js/app.min.js')
 	.options({
 		processCssUrls: false
+	})
+	// HTML Compiler
+	.html({
+		htmlRoot: './resources/index.html', // Your html root file(s)
+		output: '', // The html output folder
+		partialRoot: './resources/partials',    // default partial path
+		layoutRoot: './resources/layouts',    // default partial path
+		inject: true,
+		minify: {
+			removeComments: true
+		}
 	});
+	// Add Layouts
+
+	// Image Optimization
+
+	// SVG Sprites
+
+	// Fonts generate
+
+	// Zip
+
+	// Deployment
